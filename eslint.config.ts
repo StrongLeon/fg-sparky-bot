6f86b66dca4720df72817182ceb858f37cc92ab6
Skylafalls <triplettran56@gmail.com>
2026-01-07 11:41:59 -0500

    typed linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,9 @@
 import stylistic from "@stylistic/eslint-plugin";
 import oxlint from "eslint-plugin-oxlint";
 import { type Config, defineConfig, globalIgnores } from "eslint/config";
-import tseslint from "typescript-eslint";
 
 const _default: Config[] = defineConfig(
   globalIgnores(["node_modules", "dist"]),
-  tseslint.configs.strictTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        tsconfigRootDir: import.meta.dirname,
-        projectService: true,
-      },
-    },
-  },
   stylistic.configs.customize({
     braceStyle: "1tbs",
     semi: true,
