5fbc22fb19e13064353eaed776cb928247e516f2
Skylafalls <triplettran56@gmail.com>
2026-01-15 16:38:24 -0500

    initial stuff
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

---
+++ packages/client/src/commands/gift.ts
@@ -0,0 +1,131 @@
+/**
+ * @license
+ * fg-sparky-bot - Guess the FG number based on its symbol
+ * Copyright (C) 2025 Skylafalls
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+import { getUser } from "@fg-sparky/server";
+import type { Command } from "@fg-sparky/utils";
+import {
+  ApplicationCommandOptionType,
+  type Client,
+  type CommandInteraction,
+  type Interaction,
+  MessageFlags,
+  userMention,
+} from "discord.js";
+import { createButtonRow } from "./gift/buttons.ts";
+
+const Gift: Command = {
+  async run(
+    _client: Client,
+    interaction: CommandInteraction<"raw" | "cached">
+  ): Promise<void> {
+    if (!interaction.isChatInputCommand()) return;
+    const amount = interaction.options.getNumber("amount", true);
+    const user = interaction.options.getUser("user", true);
+    const userInDB = await getUser(user.id, interaction.guildId);
+    const giftingUser = await getUser(user.id, interaction.guildId);
+
+    if (!giftingUser) {
+      await interaction.reply({
+        content: `You don't even have a profile, go play FG sparky first!`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    if (giftingUser.tokens < amount) {
+      await interaction.reply({
+        content: `You don't have enough tokens to gift. You currently have ${giftingUser.tokens}.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    if (!userInDB) {
+      await interaction.reply({
+        content: `User ${userMention(
+          user.id
+        )} doesn't have a profile with FG sparky.`,
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+
+    const content = [
+      `User ${userMention(interaction.user.id)} wants to gift ${userMention(
+        user.id
+      )} ${amount} <:terminusfinity:1444859277515690075>.`,
+      "A tax of 5% will be applied. Do you accept?",
+    ].join("\n");
+
+    await interaction.reply({
+      content,
+      components: [createButtonRow()],
+    });
+
+    const handler = async (interact: Interaction) => {
+      if (
+        interact.id !== "gift-accept-button" &&
+        interact.id !== "gift-reject-button" &&
+        !interact.isButton()
+      ) {
+        return;
+      }
+      clearTimeout(timeout);
+      if (interact.id === "gift-accept-button") {
+        userInDB.tokens += Math.floor(amount * 0.95);
+        giftingUser.tokens -= amount;
+        await userInDB.save();
+        await interaction.editReply({
+          components: [createButtonRow(false)],
+        });
+        await interaction.followUp(
+          // dprint-ignore
+          `${userMention(
+            user.id
+          )} has accepted your gift. I wish you two a happy life together.`
+        );
+      } else {
+        await interaction.editReply({
+          components: [createButtonRow(false)],
+        });
+        await interaction.followUp(
+          `${userMention(user.id)} has dumped your tokens. Sorry about that.`
+        );
+      }
+      client.off("interactionCreate", handler);
+    };
+
+    const timeout = setTimeout(async () => {
+      client.off("interactionCreate", handler);
+      await interaction.editReply({
+        components: [createButtonRow(false)],
+      });
+      await interaction.followUp(
+        `Sorry ${userMention(interaction.user.id)}, they ghosted you.`
+      );
+    }, 5 * 60 * 1000);
+
+    client.on("interactionCreate", handler);
+  },
+  description: "Gift a person some of your tokens",
+  name: "gift",
+  options: [
+    {
+      name: "amount",
+      description: "How much to send the person",
+      type: ApplicationCommandOptionType.Number,
+      required: true,
+    },
+    {
+      name: "user",
+      description: "The person to send the gift to",
+      type: ApplicationCommandOptionType.User,
+      required: true,
+    },
+  ],
+};
+
+export default Gift;
